d09f930da4ed91a98c32c6b60bbfa2b2831045fe
root <root@ip-172-31-37-47.us-west-2.compute.internal>
2019-01-13 17:43:00 +0000

    changing default config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   basePath: './repos/C-CDA-Examples', // This is the name of the repository which is expected for query
-  schematronPath: './CCDA21.sch', // This is the schematron file expected in validator directory
+  schematronPath: './CCDA_2.1.sch', // This is the schematron file expected in validator directory
   vocPath: './voc.xml', // This is the voc XML file expected in validator directory
   server: {
     https: false
